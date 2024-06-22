eb983a875e3da0872bebe7e592826e5dfa9338a0
dabeenryu <dbryu6@gmail.com>
2024-06-22 18:47:56 +0900

    init

---
+++ src/main/java/teamseven/echoeco/EchoecoApplication.java
@@ -0,0 +1,13 @@
+package teamseven.echoeco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teamseven/echoeco/EchoecoApplicationTests.java
@@ -0,0 +1,13 @@
+package teamseven.echoeco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
