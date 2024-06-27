01218e6e9b704352b784ba242524209b6e83150b
Yunhyeok Choi <newh08@gmail.com>
2024-06-28 00:14:27 +0900

    feat: 카카오 로그인 구현

--- src/main/java/teamseven/echoeco/user/OAuthAttributes.java
+++ src/main/java/teamseven/echoeco/user/OAuthAttributes.java
@@ -29,6 +29,9 @@ public class OAuthAttributes {
         if("naver".equals(registrationId)) {
             return ofNaver("id", attributes);
         }
+        if("kakao".equals(registrationId)) {
+            return ofKakao("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -52,6 +55,20 @@ public class OAuthAttributes {
                 .email((String) response.get("email"))
                 .picture((String) response.get("profile_image"))
                 .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName,
+                                           Map<String, Object> attributes) {
+        Map<String, Object> kakaoAccount = (Map<String, Object>)attributes.get("kakao_account");
+        Map<String, Object> kakaoProfile = (Map<String, Object>)kakaoAccount.get("profile");
+
+        return OAuthAttributes.builder()
+                .name((String) kakaoProfile.get("nickname"))
+                .picture((String) kakaoProfile.get("profile_image"))
+                .email("null") // 카카오는 이메일 정보 받으려면 비즈니스 심사를 받아야해 null로 저장
+                .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
