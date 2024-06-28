df5ad8a700539541f294d02ca1edd8979d943782
Yunhyeok Choi <newh08@gmail.com>
2024-06-28 23:27:26 +0900

    feat: logout 구현

--- src/main/java/teamseven/echoeco/login/LoginController.java
+++ src/main/java/teamseven/echoeco/login/LoginController.java
@@ -1,9 +1,12 @@
 package teamseven.echoeco.login;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import teamseven.echoeco.config.auth.LoginUser;
 import teamseven.echoeco.user.User;
 
@@ -13,16 +16,11 @@ public class LoginController {
 
     @GetMapping("/")
     public String index(Model model, @LoginUser User user) {
-        if (user != null) {
-            model.addAttribute("userName", user.getName());
-        }
-
         return "index";
     }
 
     @GetMapping("/user/login")
     public String login(Model model) {
-
         return "loginPage";
     }
 
@@ -31,8 +29,17 @@ public class LoginController {
         return "index";
     }
 
-    @GetMapping("/test")
+    @GetMapping("/rr")
     public String tt() {
         return "test";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/";
     }
 }
