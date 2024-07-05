aadd1526cff41b20107ea2f4585b223db8eccffb
Sejun Park <pkt369@naver.com>
2024-07-05 23:45:44 +0900

    background 작업중

---
+++ src/main/java/teamseven/echoeco/admin/background/domain/Background.java
@@ -0,0 +1,47 @@
+package teamseven.echoeco.admin.background.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import teamseven.echoeco.admin.character.domain.Environment;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ToString
+public class Background {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "background_id")
+    private Long id;
+
+    private String name;
+
+    @Column(nullable = false)
+    private String backgroundImage;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Environment environment;
+
+    @Column(nullable = false)
+    private int level;
+
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Builder.Default
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public static Background empty() {
+        return Background.builder()
+                .name("")
+                .backgroundImage("/images/vendor/icons/unicons/chart.png")
+                .environment(Environment.CLEAN)
+                .level(0)
+                .build();
+    }
+}
---
+++ src/test/java/teamseven/echoeco/admin/background/service/BackgroundServiceTest.java
@@ -0,0 +1,130 @@
+package teamseven.echoeco.admin.background.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import teamseven.echoeco.admin.character.domain.Environment;
+import teamseven.echoeco.admin.background.domain.Background;
+import teamseven.echoeco.admin.background.repository.BackgroundRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+class BackgroundServiceTest {
+    @Autowired
+    private BackgroundRepository backgroundRepository;
+
+    private BackgroundService backgroundService;
+
+    @BeforeEach
+    void setUp() {
+        backgroundService = new BackgroundService(backgroundRepository);
+    }
+
+    @Test
+    @DisplayName("엔티티 저장시 잘 저장이 되어야 한다.")
+    public void givenEntity_whenSave_thenSuccess() {
+        Background background = Background.builder()
+                .name("name1")
+                .level(10)
+                .backgroundUrl("http")
+                .environment(Environment.CLEAN)
+                .build();
+
+        //when
+        backgroundRepository.save(background);
+
+        //then
+        List<Background> all = backgroundRepository.findAll();
+        assertEquals(1, all.size());
+        assertEquals(background, all.get(0));
+    }
+
+    @Test
+    @DisplayName("엔티티 전체 검색시 전체를 되돌려줘야 한다.")
+    public void givenEntities_whenFindAll_thenList() {
+        Background background1 = Background.builder()
+                .name("name1")
+                .level(10)
+                .backgroundUrl("http")
+                .environment(Environment.CLEAN)
+                .build();
+        Background background2 = Background.builder()
+                .name("name2")
+                .level(20)
+                .backgroundUrl("http")
+                .environment(Environment.TRASH)
+                .build();
+
+        backgroundRepository.save(background1);
+        backgroundRepository.save(background2);
+
+        //when
+        List<Background> all = backgroundService.findAll();
+
+        //then
+        assertEquals(2, all.size());
+        assertTrue(all.contains(background1));
+        assertTrue(all.contains(background2));
+    }
+
+    @Test
+    @DisplayName("id 로 검색시 하나가 나와야 한다.")
+    public void givenEntity_whenFindById_thenEntity() {
+        Background background1 = Background.builder()
+                .name("name1")
+                .level(10)
+                .backgroundUrl("http")
+                .environment(Environment.CLEAN)
+                .build();
+        Background background2 = Background.builder()
+                .name("name2")
+                .level(20)
+                .backgroundUrl("http")
+                .environment(Environment.TRASH)
+                .build();
+
+        backgroundRepository.save(background1);
+        backgroundRepository.save(background2);
+
+        //when
+        Background find = backgroundService.findById(background1.getId());
+
+        //then
+        assertEquals(find, background1);
+    }
+//
+//    @Test
+//    @DisplayName("id 로 검색시 없을시 에러가 발생해야 한다.")
+//    public void givenEntity_whenFindById_thenEntity() {
+//        Background background1 = Background.builder()
+//                .name("name1")
+//                .level(10)
+//                .backgroundUrl("http")
+//                .environment(Environment.CLEAN)
+//                .build();
+//        Background background2 = Background.builder()
+//                .name("name2")
+//                .level(20)
+//                .backgroundUrl("http")
+//                .environment(Environment.TRASH)
+//                .build();
+//
+//        backgroundRepository.save(background1);
+//        backgroundRepository.save(background2);
+//
+//        //when
+//        Background find = backgroundService.findById(1L);
+//
+//        //then
+//        assertEquals(find, background1);
+//    }
+}
