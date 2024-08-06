662bebed99136971977dadc3efbe5caaf82fe61e
Sejun Park <pkt369@naver.com>
2024-08-06 22:04:29 +0900

    slf4j + logback 로깅 기능 추가

--- src/main/java/teamseven/echoeco/home/controller/AdminHomeController.java
+++ src/main/java/teamseven/echoeco/home/controller/AdminHomeController.java
@@ -1,6 +1,7 @@
 package teamseven.echoeco.home.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin")
+@Slf4j
 public class AdminHomeController {
     @GetMapping("")
     public String home() {
+        log.info("hi");
         return "admin/home";
     }
 }
